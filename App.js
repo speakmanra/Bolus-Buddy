9fcac7fb19531b315fecaa3bb63ad1b26ddd83f5
Ryan Speakman <ryanspeakman@ryans-mac-mini.lan>
2019-10-09 18:39:11 -0500

    commit

--- App.js
+++ App.js
@@ -1,53 +1,11 @@
 import React from "react";
-import { createBottomTabNavigator } from "react-navigation-tabs";
-import { createAppContainer } from "react-navigation";
-
-import Ionicons from "react-native-vector-icons/Ionicons";
-
-import Calculate from "./views/Calculate";
-import Settings from "./views/Settings";
-import Logs from "./views/Logs";
-
-const TabNavigator = createBottomTabNavigator(
-  {
-    Calculate: Calculate,
-    Logs: Logs,
-    Settings: Settings
-  },
-  {
-    initialRouteName: "Calculate",
-    defaultNavigationOptions: ({ navigation }) => ({
-      tabBarIcon: ({ focused, horizontal, tintColor }) => {
-        const { routeName } = navigation.state;
-        let IconComponent = Ionicons;
-        let iconName;
-        if (routeName === "Calculate") {
-          iconName = `ios-calculator`;
-        } else if (routeName === "Settings") {
-          iconName = `ios-options`;
-        } else if (routeName === "Logs") {
-          iconName = "ios-paper";
-        }
-
-        // You can return any component that you like here!
-        return <IconComponent name={iconName} size={25} color={tintColor} />;
-      }
-    }),
-    tabBarOptions: {
-      activeTintColor: "green",
-      inactiveTintColor: "gray"
-    }
-  }
-);
+import BottomNav from "./Components/BottomNav";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import { View } from "react-native";
 
 function App() {
-  return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Counter />
-      </PersistGate>
-    </Provider>
-  );
+  return <BottomNav />;
 }
 
-export default createAppContainer(TabNavigator, App);
+export default App;
--- Components/BottomNav.js
+++ Components/BottomNav.js
@@ -1,47 +1,49 @@
-import React from 'react'
+import React, { Component } from "react";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import { createAppContainer } from "react-navigation";
 
 import Ionicons from "react-native-vector-icons/Ionicons";
 
-import Calculate from "./views/Calculate";
-import Settings from "./views/Settings";
-import Logs from "./views/Logs";
+import Calculate from "../views/Calculate";
+import Settings from "../views/Settings";
+import Logs from "../views/Logs";
 
+const TabNavigator = createBottomTabNavigator(
+  {
+    Calculate: Calculate,
+    Logs: Logs,
+    Settings: Settings
+  },
+  {
+    initialRouteName: "Calculate",
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let IconComponent = Ionicons;
+        let iconName;
+        if (routeName === "Calculate") {
+          iconName = `ios-calculator`;
+        } else if (routeName === "Settings") {
+          iconName = `ios-options`;
+        } else if (routeName === "Logs") {
+          iconName = "ios-paper";
+        }
 
+        // You can return any component that you like here!
+        return <IconComponent name={iconName} size={25} color={tintColor} />;
+      }
+    }),
+    tabBarOptions: {
+      activeTintColor: "green",
+      inactiveTintColor: "gray"
+    }
+  }
+);
 
-export default function BottomNav() {
-    return (
-        const TabNavigator = createBottomTabNavigator(
-            {
-              Calculate: Calculate,
-              Logs: Logs,
-              Settings: Settings
-            },
-            {
-              initialRouteName: "Calculate",
-              defaultNavigationOptions: ({ navigation }) => ({
-                tabBarIcon: ({ focused, horizontal, tintColor }) => {
-                  const { routeName } = navigation.state;
-                  let IconComponent = Ionicons;
-                  let iconName;
-                  if (routeName === "Calculate") {
-                    iconName = `ios-calculator`;
-                  } else if (routeName === "Settings") {
-                    iconName = `ios-options`;
-                  } else if (routeName === "Logs") {
-                    iconName = "ios-paper";
-                  }
-          
-                  // You can return any component that you like here!
-                  return <IconComponent name={iconName} size={25} color={tintColor} />;
-                }
-              }),
-              tabBarOptions: {
-                activeTintColor: "green",
-                inactiveTintColor: "gray"
-              }
-            }
-          );
-    )
+const AppContainer = createAppContainer(TabNavigator);
+
+export default class BottomNav extends Component {
+  render() {
+    return <AppContainer />;
+  }
 }
